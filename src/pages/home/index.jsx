486ae469d994419bd5d02b4815ef5307fd1ca757
Namoun Youcef <youcef.namoun@imadrassa.com>
2024-03-28 15:24:47 +0100

    update : display people list

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card } from 'antd';
+import { Card, List, Avatar } from 'antd';
 
 function Home() {
     const dispatch = useDispatch();
@@ -22,16 +22,56 @@ function Home() {
         })
     }, []);
 
-    return (
-        <div>
-            <h1>People List</h1>
-            <Card>
-                {peoples?.map((people) => (
-                    <div key={people.id}>{people.firstname} {people.lastname}</div>
+    const data = [
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 2',
+        },
+        {
+            title: 'Ant Design Title 3',
+        },
+        {
+            title: 'Ant Design Title 4',
+        },
+    ];
 
-                ))}
-            </Card>
-        </div>
+    function getAge(birthDate) {
+        const today = new Date();
+        const birthDateObj = new Date(birthDate);
+        const age = today.getFullYear() - birthDateObj.getFullYear();
+
+        // Ajuster l'âge si le jour de naissance n'est pas encore passé cette année
+        if (today.getMonth() < birthDateObj.getMonth()) {
+            age--;
+        } else if (today.getMonth() === birthDateObj.getMonth() && today.getDate() < birthDateObj.getDate()) {
+            age--;
+        }
+
+        return age;
+    }
+    return (
+        <Card style={{ width: '60%' }}>
+            <h1>Liste des personnes</h1>
+            <List
+                pagination={{
+                    defaultPageSize: 8,
+                }}
+                dataSource={peoples}
+                renderItem={(item, index) => (
+                    <List.Item>
+                        <List.Item.Meta
+                            avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+                            title={<a href="https://ant.design">{item.firstname} {item.lastname} </a>}
+                            description={`${getAge(item?.birthyear)} ans`}
+                            extra={<p>yanis</p>}
+                        />
+                        <div>content</div>
+                    </List.Item>
+                )}
+            />
+        </Card>
     )
 }
 
